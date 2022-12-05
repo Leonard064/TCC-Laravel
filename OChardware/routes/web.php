557b4e2c769e0cc21c5035791895de250f32c51e
Leonard064 <leonardo.nascibarbosa@outlook.com>
2022-12-05 13:47:23 -0300

    Inserção de rotas para controllers Categoria, Marca, Avaliacao e Endereco

--- OChardware/routes/web.php
+++ OChardware/routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 use App\http\Controllers\UsuarioController;
 use App\Http\Controllers\Prod_carrinhoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\AvaliacaoController;
+use App\Http\Controllers\EnderecoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,7 +34,7 @@ Route::get('/produtos/{categoria}',[ProdutoController::class, 'showCat']);
 
 */
 
-Route::get('/produtos/{categoria?}',[ProdutoController::class, 'showProdutos']);
+Route::get('/produtos/{id_categoria?}',[ProdutoController::class, 'showProdutos']);
 
 Route::get('/detalhes/{id}', [ProdutoController::class, 'show']);
 
@@ -38,6 +42,16 @@ Route::get('/create-produto',[ProdutoController::class, 'create']);
 
 Route::post('/produtos/create', [ProdutoController::class, 'store']);
 
+
+// --- Categoria ---
+Route::get('/create-categoria', [CategoriaController::class, 'create']);
+
+Route::post('/categorias/create', [CategoriaController::class, 'store']);
+
+// --- Marca ---
+Route::get('/create-marca', [MarcaController::class, 'create']);
+
+Route::post('/marcas/create', [MarcaController::class, 'store']);
 
 
 // --- Carrinho ---
@@ -47,12 +61,23 @@ Route::post('/addCarrinho',[Prod_carrinhoController::class, 'addCarrinho']);
 
 Route::get('/removerCarrinho/{id}', [Prod_carrinhoController::class, 'removerCarrinho']);
 
+Route::post('/checkout',[Prod_carrinhoController::class, 'showCheckout']);
 
+// --- Endereco ---
+Route::post('/endereco/create', [EnderecoController::class, 'addEnderecoPerfil']);
+
+Route::post('/endereco/create-via-checkout', [EnderecoController::class, 'addEnderecoCheckout']);
+
+
+// --- Avaliacao ---
+Route::post('/avaliacoes/create', [AvaliacaoController::class, 'store']);
 
 // --- Usuários ---
 
 //Criação, autenticação e logoff
-Route::get('/login',[UsuarioController::class, 'create']);
+Route::get('/login',[UsuarioController::class, 'showLogin']);
+
+Route::get('/cadastre-se',[UsuarioController::class, 'showCadastre']);
 
 Route::post('/registrar',[UsuarioController::class, 'store']);
 
@@ -60,9 +85,15 @@ Route::post('/entrar',[UsuarioController::class, 'authenticate']);
 
 Route::get('/logout',[UsuarioController::class, 'logout']);
 
+
 //Rotas nível usuário
 
 Route::get('/perfil',[UsuarioController::class, 'perfil']);
+
+Route::get('/editar-perfil',[UsuarioController::class, 'showEditPerfil']);
+
+Route::get('/adicionar-endereco', [EnderecoController::class, 'showAddEndereco']);
+
 
 //Rotas nível Admin
 
