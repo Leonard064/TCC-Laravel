a41f4ac75f8c913843b6a7c9fd99a4d9d825d0fb
Leonard064 <leonardo.nascibarbosa@outlook.com>
2022-12-05 13:46:18 -0300

    Inserção de campos dinâmicos na aba "Busca Específica"

--- OChardware/resources/views/produtos.blade.php
+++ OChardware/resources/views/produtos.blade.php
@@ -10,26 +10,35 @@
                 <form action="" method="post">
 
                     <h3>Marcas</h3>
-                    @for ($i = 0; $i < 9; $i++)
-                        <input type="radio" name="marca" id="marca"><label for="marca">Marca {{$i}}</label><br>
-                    @endfor
+
+                        @if($marca)
+
+                            @foreach ($marca as $marcas)
+                                <input type="radio" name="marca" id="{{$marcas->nome}}" value="{{$marcas->id}}">
+                                <label for="{{$marcas->nome}}">{{$marcas->nome}}</label><br>
+                            @endforeach
+
+                        @else
+
+                            <h4>Não há marcas</h4>
+                        @endif
 
                     <h3>Seção</h3>
-                    <input type="radio" name="processador" id="processador"><label for="processador">Processador</label><br>
-                    <input type="radio" name="placa-mae" id="placa-mae"><label for="placa-mae">Placas-Mãe</label><br>
-                    <input type="radio" name="placa-de-video" id="placa-de-video"><label for="placa-de-video">Placas de Vídeo</label><br>
-                    <input type="radio" name="" id="memoria"><label for="memoria">Memória</label><br>
-                    <input type="radio" name="" id="monitor"><label for="monitor">Monitor</label><br>
-                    <input type="radio" name="" id="mouse-teclado"><label for="mouse-teclado">Mouse e Teclado</label><br>
-                    <input type="radio" name="" id="hd-ssd"><label for="hd-ssd">HD e SSD</label><br>
-                    <input type="radio" name="" id="fonte"><label for="fonte">Fonte</label><br>
+                        @if (count($categoria)<=0)
+                            <h4>Não há categorias</h4>
+                        @else
+                            @foreach ($categoria as $categorias)
+                                <input type="radio" name="categoria" id="{{$categorias->nome}}" value="{{$categorias->id}}">
+                                <label for="{{$categorias->nome}}">{{$categorias->nome}}</label><br>
+                            @endforeach
+                        @endif
 
                     <h3>Preço</h3>
 
-                    <input type="checkbox" name="" id="baixo"><label for="baixo">R$0,00 até R$100,00</label><br>
-                    <input type="checkbox" name="" id="medio"><label for="medio">R$100,00 até R$500,00</label><br>
-                    <input type="checkbox" name="" id="alto"><label for="alto">R$500,00 até R$1000,00</label><br>
-                    <input type="checkbox" name="" id="caro"><label for="caro">Acima de R$1000,00</label><br><br>
+                    <input type="radio" name="valor" id="baixo"><label for="baixo">R$0,00 até R$100,00</label><br>
+                    <input type="radio" name="valor" id="medio"><label for="medio">R$100,00 até R$500,00</label><br>
+                    <input type="radio" name="valor" id="alto"><label for="alto">R$500,00 até R$1000,00</label><br>
+                    <input type="radio" name="valor" id="caro"><label for="caro">Acima de R$1000,00</label><br><br>
 
                     <button class="bt-red">Pesquisar</button>
 
@@ -42,17 +51,23 @@
             @if($pesquisa)
                 <h2>Resultados para "{{$pesquisa}}"</h2>
             @elseif($categoria)
-                <h2>Resultados para "{{$categoria}}"</h2>
+                <h2>Resultados para "{{$categoria[0]->nome}}"</h2>
             @endif
 
             <div class="flex-container itens">
 
                 @if(count($produto) === 0 && $pesquisa)
+
                     <h1>A pesquisa não encontrou resultados.</h1>
+
                 @elseif(count($produto) === 0)
+
                     <h1>Não há produtos.</h1>
+
                 @else
+
                     @include('layouts._produtos')
+
                 @endif
 
 
