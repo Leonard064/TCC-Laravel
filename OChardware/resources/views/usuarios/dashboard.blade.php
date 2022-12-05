7dec782556190a1685a53c342eaf5ea217edca34
Leonard064 <leonardo.nascibarbosa@outlook.com>
2022-12-05 13:45:29 -0300

    Inserção de campos dinâmicos em Perfil e Dashboard - Adm

--- OChardware/resources/views/usuarios/dashboard.blade.php
+++ OChardware/resources/views/usuarios/dashboard.blade.php
@@ -4,8 +4,45 @@
 
 @section('conteudo')
     <div class="flex-container corpo margem">
-        <h1>Admin</h1>
-        <a href="/create-produto">Criar Produto</a>
-        <a href="">Criar Frete</a>
+        <div>
+            <h1>Admin</h1>
+            <a href="/create-categoria">Criar Categoria</a>
+            <a href="/create-marca">Criar Marca</a>
+            <a href="/create-produto">Criar Produto</a>
+        </div>
+
+        <div class="flex-container secao">
+            <h3>Pedidos Abertos</h3>
+        </div>
+
+        <div class="detalhes-descricao margem">
+            @if (count($pedidos) > 0)
+
+                @foreach ($pedidos as $pedido)
+                    <table>
+                        <caption>Pedido: {{$pedido->id}}</caption>
+                        <tr>
+                            <th>Data e hora</th>
+                            <th>Pagamento</th>
+                            <th>Valor</th>
+                            <th>Status</th>
+                        </tr>
+                        <tr>
+                            <td>{{$pedido->created_at}}</td>
+                            <td>{{$pedido->pagamento_tipo}}</td>
+                            <td>{{$pedido->total_pedido}}</td>
+                            <td>{{$pedido->status}}</td>
+                            <td><a href="#">Editar Status</a></td>
+                        </tr>
+                    </table>
+                @endforeach
+
+            @else
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                <h2>Ainda não foram realizados Pedidos</h2>
+            @endif
+        </div>
+
     </div>
+
 @endsection
--- OChardware/resources/views/usuarios/perfil.blade.php
+++ OChardware/resources/views/usuarios/perfil.blade.php
@@ -3,5 +3,53 @@
 @section('title', 'Meu Perfil - OCHardware')
 
 @section('conteudo')
-    <h1>Página meu perfil</h1>
+    <section class="flex-container corpo-detalhes">
+        <section class="detalhes-topo">
+            <div class="grid-container detalhes-foto-nome margem">
+                <div class="flex-container detalhes-foto">
+                    <img src="img/blank-profile-picture-g03979cc6f_640.png" alt="Image by Stephanie Edwards pixabay.com">
+                </div>
+                <div class="flex-container detalhes-nome">
+                    <h2>{{Auth::user()->nome}} {{Auth::user()->sobrenome}}</h2>
+                    <hr>
+                    <h3>Usuário desde: {{Auth::user()->created_at}}</h3>
+                    <a href="/editar-perfil">Editar cadastro</a>
+                    <a href="/adicionar-endereco">Meus Endereços</a>
+                    {{-- <a href="#">Editar cadastro</a> --}}
+                </div>
+            </div>
+        </section>
+
+        <div class="flex-container secao">
+            <h3>Meus Pedidos</h3>
+        </div>
+        <div class="detalhes-descricao margem">
+
+            @if (count($pedidos) > 0)
+
+                @foreach ($pedidos as $pedido)
+                    <table>
+                        <caption>Pedido: {{$pedido->id}}</caption>
+                        <tr>
+                            <th>Data e hora</th>
+                            <th>Pagamento</th>
+                            <th>Valor</th>
+                            <th>Status</th>
+                        </tr>
+                        <tr>
+                            <td>{{$pedido->created_at}}</td>
+                            <td>{{$pedido->pagamento_tipo}}</td>
+                            <td>{{$pedido->total_pedido}}</td>
+                            <td>{{$pedido->status}}</td>
+                        </tr>
+                    </table>
+                @endforeach
+
+            @else
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                <h2>Ainda não foram realizadas Compras</h2>
+            @endif
+
+        </div>
+    </section>
 @endsection
