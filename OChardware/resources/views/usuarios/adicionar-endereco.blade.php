b203b2dbb417cfadb712208e420a5516dc6b74bd
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-01-01 12:24:55 -0300

    Melhorias na página "Meus Endereços"

--- OChardware/resources/views/usuarios/adicionar-endereco.blade.php
+++ OChardware/resources/views/usuarios/adicionar-endereco.blade.php
@@ -12,13 +12,21 @@
 
                         @if (count($enderecos) > 0)
 
-                            <ul>
-                            @foreach ($enderecos as $endereco)
+                            <table>
+                                <tr>
+                                    <th>Endereço</th>
+                                    <th>Estado</th>
+                                </tr>
 
-                               <li>{{$endereco->endereco}}</li>
+                                    @foreach ($enderecos as $endereco)
 
-                            @endforeach
-                            </ul>
+                                        <td>{{$endereco->endereco}}</td>
+                                        <td>{{$endereco->estado}}</td>
+                                        <td><a href="/editar-endereco/{{$endereco->id}}">Editar</a></td>
+                                        <td><a href="/remover-endereco/{{$endereco->id}}">Excluir</a></td>
+
+                                    @endforeach
+                            </table>
 
                         @else
                             <h3>Você ainda não possui endereço cadastrado.</h3>
@@ -32,21 +40,28 @@
             <h3>Adicionar Novo Endereço</h3>
         </div>
 
-        <div class="flex-container corpo margem">
-            <form action="/endereco/create" method="post">
-                @csrf
+        <div class="grid-container forms margem">
+
+            {{-- Endereços Salvos --}}
+
+            <div class="flex-container form">
+
+                <form action="/endereco/create" method="POST">
+                    @csrf
+
+                    <input type="text" name="cep" id="cep" placeholder="cep" class="input-full">
+                    <input type="text" name="endereco" id="endereco" placeholder="endereco" class="input-full">
+                    <input type="text" name="numero" id="numero" placeholder="numero" class="input-full">
+                    <input type="text" name="bairro" id="bairro" placeholder="bairro" class="input-full">
+                    <input type="text" name="estado" id="estado" placeholder="estado" class="input-full">
+                    <input type="text" name="municipio" id="municipio" placeholder="municipio" class="input-full">
 
 
-                <input type="text" name="cep" id="cep" placeholder="cep">
-                <input type="text" name="endereco" id="endereco" placeholder="endereco">
-                <input type="text" name="numero" id="numero" placeholder="numero">
-                <input type="text" name="bairro" id="bairro" placeholder="bairro">
-                <input type="text" name="estado" id="estado" placeholder="estado">
-                <input type="text" name="municipio" id="municipio" placeholder="municipio">
+                    <button class="bt-red">Adicionar</button>
 
-                <button class="bt-red">Adicionar</button>
+                </form>
+            </div>
 
-            </form>
         </div>
 
     </section>
