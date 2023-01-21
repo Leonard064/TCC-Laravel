67fe4911a7517a4b511360901ecc1e8839d38eb6
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-01-21 14:22:26 -0300

    Iniciado inserção de código funcional

--- OChardware/app/Http/Controllers/FreteController.php
+++ OChardware/app/Http/Controllers/FreteController.php
@@ -7,36 +7,51 @@ use Illuminate\Http\Request;
 use EscapeWork\Frete\Correios\PrecoPrazo;
 use EscapeWork\Frete\Correios\Data;
 use EscapeWork\Frete\FreteException;
+use App\Models\Endereco;
+use App\Http\Controllers\Prod_CarrinhoController;
 
 class FreteController extends Controller
 {
 
     //API de retorno de fretes
-    public function calculoFrete(Request $request){
+    public function calculoFrete($id){
 
-        $teste = new PrecoPrazo();
-        $teste->setCodigoServico([Data::SEDEX, Data::PAC])
-                ->setCodigoEmpresa('08082650')
-                ->setSenha('564321')
-                ->setCepOrigem('07500000')
-                ->setCepDestino('21030001')
-                ->setComprimento(30)
-                ->setAltura(30)
-                ->setLargura(30)
-                ->setDiametro(30)
-                ->setPeso(0.5);
+        try {
+            $frete = Endereco::findOrFail($id);
 
-            try {
-                $result = $teste->calculate();
+            $calculoFrete = new PrecoPrazo();
+            $calculoFrete->setCodigoServico([Data::PAC])
+                    ->setCodigoEmpresa('08082650')
+                    ->setSenha('564321')
+                    ->setCepOrigem('07500000')
+                    ->setCepDestino($frete->cep)
+                    ->setComprimento(30)
+                    ->setAltura(30)
+                    ->setLargura(30)
+                    ->setDiametro(30)
+                    ->setPeso(0.5);
 
-                return response()->json([
-                    'data' => $result['cServico']
-                ]);
+                try {
+                    $result = $calculoFrete->calculate();
 
-            } catch (FreteException $e) {
-                echo $e->getMessage();
-                echo $e->getCode();
-            }
+                    //dd($result['cServico']);
+
+                    // return Prod_CarrinhoController::showCheckout($result);
+
+                    return response()->json([
+                        'data' => $result['cServico']
+                        // 'data' => $request
+                    ]);
+
+                } catch (FreteException $e) {
+                    echo $e->getMessage();
+                    echo $e->getCode();
+                }
+
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+
+        }
 
     }
 
