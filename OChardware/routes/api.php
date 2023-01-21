3aab2181a78a657c94249dc665cf3bf88d41182f
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-01-21 14:24:23 -0300

    Modificado rota "Cálculo de Frete"

--- OChardware/routes/api.php
+++ OChardware/routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/frete', [FreteController::class,'calculoFrete']);
+Route::post('/frete/{id}', [FreteController::class,'calculoFrete']);
