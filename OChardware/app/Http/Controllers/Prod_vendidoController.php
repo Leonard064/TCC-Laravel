a935329e028545651cc10537d67a9d60decfb20c
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-02-17 22:03:26 -0300

    Mudanças - corrigido pedidos +1 itens

--- OChardware/app/Http/Controllers/Prod_vendidoController.php
+++ OChardware/app/Http/Controllers/Prod_vendidoController.php
@@ -8,45 +8,54 @@ use Illuminate\Http\Request;
 
 class Prod_vendidoController extends Controller
 {
+
     public static function store($id_pedido){
 
         //pega todos os itens do carrinho no banco
-        //--- chama a função 'carrinhoItens' do controller 'Prod_Carrinho' ---
-        $produtos = app(Prod_carrinhoController::class)->carrinhoItens();
+        // $produtos = app(Prod_carrinhoController::class)->carrinhoItens();
+        //refiz a função pois dava conflito
+        try {
+
+            $produtos = Prod_Carrinho::where('id_usuario', \Auth::user()->id)
+                                            ->join('produtos','prod__carrinhos.id_produto', '=','produtos.id')
+                                            ->select('prod__carrinhos.*','produtos.nome','produtos.preco','produtos.foto','produtos.peso')
+                                            ->get();
 
 
-        //para cada item ele cria um registro na tabela "Produtos Vendidos"
-        foreach ($produtos as $produto){
 
-            $prod_vendido = new Prod_vendido;
+            foreach ($produtos as $produto){
 
-            //(por enquanto) O resultado está voltando em um array dentro de array
-            //por isso o [0]
-            $prod_vendido->valor_unitario = $produto[0]->preco;
-            $prod_vendido->quantidade = $produto[0]->quantidade;
-            $prod_vendido->id_pedido = $id_pedido;
-            $prod_vendido->id_produto = $produto[0]->id_produto;
+                $prod_vendido = new Prod_vendido;
 
-                try {
+                //(por enquanto) O resultado está voltando em um array dentro de array
+                //por isso o [0]
+                $prod_vendido->valor_unitario = $produto->preco;
+                $prod_vendido->quantidade = $produto->quantidade;
+                $prod_vendido->id_pedido = $id_pedido;
+                $prod_vendido->id_produto = $produto->id_produto;
+
+
                     \DB::beginTransaction();
                         $prod_vendido->save();
                         \DB::commit();
 
                             //Após o processo de registro da compra, o carrinho é esvaziado
-                            Prod_Carrinho::destroy($produto[0]->id);
+                            Prod_Carrinho::destroy($produto->id);
 
-                } catch (\Throwable $th) {
-                    echo $th->getMessage();
-                        \DB::rollback();
 
-                }
+            }
+
+
+        }catch (\Throwable $th) {
+            return $th->getMessage();
+
         }
+
 
         //Redireciona ao Index
         //--- depois criar página de sucesso ---
         \Session::flash('ok','Pedido concluído!');
         return redirect('/');
-
 
     }
 }
