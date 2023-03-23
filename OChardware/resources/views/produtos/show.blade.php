a68c11e5cf6aa16473612dabd0bce4df66f2c041
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-03-23 18:51:37 -0300

    Mudanças - Adicionado estilo para pág. Detalhes

--- OChardware/resources/views/produtos/show.blade.php
+++ OChardware/resources/views/produtos/show.blade.php
@@ -4,8 +4,8 @@
 
 @section('conteudo')
     <section class="flex-container corpo-detalhes">
-        <section class="detalhes-topo">
-            <div class="grid-container detalhes-foto-nome margem">
+        <section class="detalhes-topo bg-gray border-10 margin-new">
+            <div class="grid-container detalhes-foto-nome">
                 <div class="flex-container detalhes-foto">
                     <img src="../img/produtos/{{$produto->foto}}" alt="OverClock">
                 </div>
@@ -20,7 +20,7 @@
                         <h2>{{$produto->nome}}</h2>
                         <hr>
                         <h3>{{$produto->quantidade}} Peças disponíveis</h3>
-                        <h3>R${{$produto->preco}}</h3>
+                        <h2 class="label-preco red">R${{$produto->preco}}</h2>
                         <h4>Valor em pix/ à vista no cartão</h4>
                         <h4>10x de 99,90 sem juros</h4>
                         <form action="/addCarrinho" method="post">
@@ -35,13 +35,83 @@
             </div>
         </section>
 
-        <div class="flex-container secao black">
+        {{-- FIXME - Acertar as divs - precisam ser separadas --}}
+        <div class="detalhes-descricao bg-gray border-10 padding-detalhes margin-new">
+            <i class="fa-solid fa-square red"></i> &nbsp;
+            <h3>Descrição</h3>
+            <p>{{$produto->descricao}}</p>
+        </div>
+
+        <div class="detalhes-comentarios-avaliacao bg-gray border-10 padding-detalhes margin-new">
+            <i class="fa-solid fa-square red"></i> &nbsp;
+            <h3>Avaliações</h3>
+
+            <div class="grid-container grid-comentarios-avaliacao">
+                <div class="detalhes-comentarios">
+                    <i class="fa-solid fa-square red"></i>
+                    <h3>Comentários</h3>
+
+                    @if(count($avaliacao) > 0)
+
+                        @foreach ($avaliacao as $avaliacao)
+                            <div class="grid-container detalhes-foto-nome">
+                                <div>
+
+                                    @if($avaliacao->gostou)
+                                        <i class="fa-solid fa-thumbs-up"></i>
+                                    @else
+                                        <i class="fa-solid fa-thumbs-down"></i>
+                                    @endif
+
+                                </div>
+                                <div>
+                                    <h4>{{$avaliacao->nome}} {{$avaliacao->sobrenome}}</h4>
+                                    <p>{{$avaliacao->texto_avaliacao}}</p>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    @else
+                        <h4>Ainda não há avaliações para o produto</h4>
+                    @endif
+
+                </div>
+
+                @if(Auth::check())
+                    <div class="detalhes-avaliacao">
+                        <i class="fa-solid fa-square red"></i>
+                        <h3>Gostou do produto?</h3>
+                        <form action="/avaliacoes/create" method="post">
+                        @csrf
+
+                            {{-- manda os ids de cliente e produto --}}
+                            <input type="hidden" name="id_usuario" value="{{Auth::user()->id}}">
+                            <input type="hidden" name="id_produto" value="{{$produto->id}}">
+
+                            <input type="radio" name="gostou" value="1" >Adorei!<br>
+                            <input type="radio" name="gostou" value="0" >Odiei!<br>
+                            <label for="texto">Deixe sua avaliação: (máx. 100 caracteres)</label>
+                            <textarea name="texto_avaliacao" id="texto" cols="80" rows="10"></textarea>
+                            <button class="bt-red">Enviar</button>
+
+                        </form>
+                    </div>
+                @endif
+            </div>
+
+        </div>
+
+        {{-- Comentado para evitar possiveis problemas
+
+            <div class="flex-container secao black">
             <i class="fa-solid fa-square red"></i> &nbsp;
             <h3>Descrição</h3>
         </div>
         <div class="detalhes-descricao margem">
             <p>{{$produto->descricao}}</p>
         </div>
+
+        -- comentarios e avaliaçoes
 
         <div class="flex-container secao black">
             <i class="fa-solid fa-square red"></i> &nbsp;
@@ -88,7 +158,7 @@
                                 <p>{{$avaliacao[$i]->texto_avaliacao}}</p>
                             </div>
                         </div>
-                    @endfor --}}
+                    @endfor
 
 
                 @else
@@ -103,7 +173,7 @@
                     <form action="/avaliacoes/create" method="post">
                     @csrf
 
-                        {{-- manda os ids de cliente e produto --}}
+                        {{-- manda os ids de cliente e produto
                         <input type="hidden" name="id_usuario" value="{{Auth::user()->id}}">
                         <input type="hidden" name="id_produto" value="{{$produto->id}}">
 
@@ -117,7 +187,7 @@
                 </div>
             @endif
 
-        </div>
+        </div>--}}
 
     </section>
 @endsection
