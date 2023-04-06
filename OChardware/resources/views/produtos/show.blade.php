224e99ed4c74d737950d7b65c7fadc39278907b3
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-04-06 13:01:44 -0300

    Mudanças - Layout Detalhes

--- OChardware/resources/views/produtos/show.blade.php
+++ OChardware/resources/views/produtos/show.blade.php
@@ -10,24 +10,28 @@
                     <img src="../img/produtos/{{$produto->foto}}" class="border-10" alt="OverClock">
                 </div>
                 <div class="flex-container detalhes-nome">
+
+                    <h1>{{$produto->nome}}</h1>
+
                     @if($produto->quantidade <= 0)
 
-                        <h2>{{$produto->nome}}</h2>
                         <h3>OPS! Não possuimos estoque desse produto.</h3>
 
                     @else
 
-                        <h2>{{$produto->nome}}</h2>
                         <hr>
-                        <h3>{{$produto->quantidade}} Peças disponíveis</h3>
-                        <h2 class="label-preco red">R${{$produto->preco}}</h2>
-                        <h4>Valor em pix/ à vista no cartão</h4>
-                        <h4>10x de 99,90 sem juros</h4>
-                        <form action="/addCarrinho" method="post">
-                            @csrf
-                            <input type="hidden" name="id" value="{{$produto->id}}">
-                            <button class="bt-red">Comprar</button>
-                        </form>
+                        <div class="flex-container reverso">
+                            <h3>{{$produto->quantidade}} Peças disponíveis</h3>
+                            <h1 class="label-preco red">R$ {{number_format($produto->preco,2,',','.')}}</h1>
+                            {{-- <h4>Valor em pix/ à vista no cartão</h4>
+                            <h4>10x de 99,90 sem juros</h4> --}}
+                            <form action="/addCarrinho" method="post">
+                                @csrf
+                                <input type="hidden" name="id" value="{{$produto->id}}">
+                                <button class="bt-red">Comprar</button>
+                            </form>
+
+                        </div>
 
                     @endif
 
@@ -37,8 +41,12 @@
 
         {{-- FIXME - Acertar as divs - precisam ser separadas --}}
         <div class="detalhes-descricao bg-gray border-10 padding-detalhes margin-new">
-            <i class="fa-solid fa-square red"></i> &nbsp;
-            <h3>Descrição</h3>
+
+            <div class="flex-container topo-secao">
+                <i class="fa-solid fa-square red"></i>
+                <h3>Descrição</h3>
+            </div>
+
             <p>{{$produto->descricao}}</p>
         </div>
 
@@ -49,8 +57,12 @@
             @if (Auth::check())
             <div class="grid-container grid-comentarios-avaliacao margin-new">
                 <div class="detalhes-comentarios bg-gray padding-detalhes border-10">
-                    <i class="fa-solid fa-square red"></i>
-                    <h3>Comentários</h3>
+
+                    <div class="flex-container topo-secao">
+                        <i class="fa-solid fa-square red"></i>
+                        <h3>Comentários</h3>
+                    </div>
+
 
                     @if(count($avaliacao) > 0)
 
@@ -80,29 +92,42 @@
 
 
                     <div class="detalhes-avaliacao bg-gray padding-detalhes border-10">
-                        <i class="fa-solid fa-square red"></i>
-                        <h3>Gostou do produto?</h3>
-                        <form action="/avaliacoes/create" method="post">
-                        @csrf
 
-                            {{-- manda os ids de cliente e produto --}}
-                            <input type="hidden" name="id_usuario" value="{{Auth::user()->id}}">
-                            <input type="hidden" name="id_produto" value="{{$produto->id}}">
+                        <div class="flex-container topo-secao">
+                            <i class="fa-solid fa-square red"></i>
+                            <h3>Gostou do produto?</h3>
+                        </div>
 
-                            <input type="radio" name="gostou" value="1" >Adorei!<br>
-                            <input type="radio" name="gostou" value="0" >Odiei!<br>
-                            <label for="texto">Deixe sua avaliação: (máx. 100 caracteres)</label>
-                            <textarea name="texto_avaliacao" id="texto" cols="80" rows="10"></textarea>
-                            <button class="bt-red">Enviar</button>
+                            <form action="/avaliacoes/create" method="post" class="flex-container detalhes-form">
+                                @csrf
 
-                        </form>
+                                    {{-- manda os ids de cliente e produto --}}
+                                    <input type="hidden" name="id_usuario" value="{{Auth::user()->id}}">
+                                    <input type="hidden" name="id_produto" value="{{$produto->id}}">
+
+                                    <div class="form-opcoes">
+                                        <input type="radio" name="gostou" value="1" >Adorei!
+                                    </div>
+                                    <div class="form-opcoes">
+                                        <input type="radio" name="gostou" value="0" >Odiei!
+                                    </div>
+
+                                    <label for="texto">Deixe sua avaliação: (máx. 100 caracteres)</label>
+                                    <textarea name="texto_avaliacao" id="texto" cols="80" rows="10" class=""></textarea>
+                                    <button class="bt-red">Enviar</button>
+
+                                </form>
+
                     </div>
 
             @else
                 <div class="grid-container grid-comentarios margin-new">
                     <div class="detalhes-comentarios bg-gray padding-detalhes border-10">
-                        <i class="fa-solid fa-square red"></i>
-                        <h3>Comentários</h3>
+
+                        <div class="flex-container topo-secao">
+                            <i class="fa-solid fa-square red"></i>
+                            <h3>Comentários</h3>
+                        </div>
 
                         @if(count($avaliacao) > 0)
 
