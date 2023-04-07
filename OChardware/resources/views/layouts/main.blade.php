0b1122ac9027d8db9de68340d770aedd29170fd4
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-04-07 13:21:28 -0300

    Mudanças - Alteração no footer da pág.

--- OChardware/resources/views/layouts/main.blade.php
+++ OChardware/resources/views/layouts/main.blade.php
@@ -182,41 +182,46 @@
         </main>
         <footer>
             <section class="flex-container rodape black">
-                <section class="flex-container rodape-secao1">
-                    <div class="rodape-secao1-info">
-                        <h4>Siga nossos perfis</h4>
-                        <span class="fa-brands fa-facebook fa-2xl"></span>
-                        <span class="fa-brands fa-instagram fa-2xl"></span>
-                        <span class="fa-brands fa-twitter fa-2xl"></span>
-                    </div>
+                <section class="margin-new">
+
+                    <section class="flex-container rodape-secao1">
+                        <div class="rodape-secao1-info">
+                            <h4>Siga nossos perfis</h4>
+                            <span class="fa-brands fa-facebook fa-2xl"></span>
+                            <span class="fa-brands fa-instagram fa-2xl"></span>
+                            <span class="fa-brands fa-twitter fa-2xl"></span>
+                        </div>
+                    </section>
+                    <section class="grid-container rodape-secao2">
+                        <div class="rodape-secao2-esquerda">
+                            <h4>Overclock® - Remodel by L064 - original de BRANDO INDUSTRIES</h4>
+                            <p>Preços e condições de pagamento exclusivos para compras via internet e podem variar nas lojas físicas. Os preços anunciados neste site ou via e-mail promocional podem ser alterados sem prévio aviso. A Overclock®, não é responsável por erros descritivos. As fotos contidas nesta página são meramente ilustrativas do produto e podem variar de acordo com o fornecedor/lote do fabricante. Ofertas válidas até o término de nossos estoques. Vendas sujeitas à análise e confirmação de dados.</p>
+                        </div>
+                        <div class="grid-container rodape-secao2-meio">
+                            <div>
+                                <h4>Formas de Pagamento</h4>
+                                <img src="../img/boleto.png" alt="">
+                                <img src="../img/hipercard.png" alt="">
+                                <img src="../img/master.png" alt="">
+                                <!-- <img src="img/paypal.jpg" alt=""> -->
+                            </div>
+                            <div>
+                                <h5>2022/2023 - Leonard064.</h5>
+                            </div>
+                        </div>
+                        <div class="grid-container rodape-secao2-direita">
+                            <div>
+                                <h4>Contato</h4>
+                                <p>SAC - (21) 9999 - 9999</p>
+                                <p>Email - atendimetooc@email.com</p>
+                                <h4>Horário de Funcionamento</h4>
+                                <p>Loja Física: de seg a sex - 08:00 às 21:00</p>
+                            </div>
+                        </div>
+                    </section>
+
                 </section>
-                <section class="grid-container rodape-secao2">
-                    <div class="rodape-secao2-esquerda">
-                        <h4>Overclock® - Remodel by L064 - original de BRANDO INDUSTRIES</h4>
-                        <p>Preços e condições de pagamento exclusivos para compras via internet e podem variar nas lojas físicas. Os preços anunciados neste site ou via e-mail promocional podem ser alterados sem prévio aviso. A Overclock®, não é responsável por erros descritivos. As fotos contidas nesta página são meramente ilustrativas do produto e podem variar de acordo com o fornecedor/lote do fabricante. Ofertas válidas até o término de nossos estoques. Vendas sujeitas à análise e confirmação de dados.</p>
-                    </div>
-                    <div class="grid-container rodape-secao2-meio">
-                        <div>
-                            <h4>Formas de Pagamento</h4>
-                            <img src="../img/boleto.png" alt="">
-                            <img src="../img/hipercard.png" alt="">
-                            <img src="../img/master.png" alt="">
-                            <!-- <img src="img/paypal.jpg" alt=""> -->
-                        </div>
-                        <div>
-                            <h5>2022/2023 - Leonard064.</h5>
-                        </div>
-                    </div>
-                    <div class="grid-container rodape-secao2-direita">
-                        <div>
-                            <h4>Contato</h4>
-                            <p>SAC - (21) 9999 - 9999</p>
-                            <p>Email - atendimetooc@email.com</p>
-                            <h4>Horário de Funcionamento</h4>
-                            <p>Loja Física: de seg a sex - 08:00 às 21:00</p>
-                        </div>
-                    </div>
-                </section>
+
             </section>
         </footer>
     </body>
