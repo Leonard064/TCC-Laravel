52c0f0fb8d10654eba39beba5c4e7a5b45a0b63b
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-04-07 13:16:44 -0300

    Mudanças - Layouts Perfil e Dashboard

--- OChardware/resources/views/usuarios/dashboard.blade.php
+++ OChardware/resources/views/usuarios/dashboard.blade.php
@@ -3,15 +3,92 @@
 @section('title','OC - Admin')
 
 @section('conteudo')
-    <div class="flex-container corpo">
-        <div class="margem" style="background-color: #d6d6d6">
-            <h1>Admin</h1>
-            <a href="/create-categoria">Criar Categoria</a>
-            <a href="/create-marca">Criar Marca</a>
+    <div class="flex-container corpo-detalhes">
+        <div class="detalhes-topo bg-gray border-10 margin-new padding-detalhes">
+            <h1>Painel Administrador</h1>
+            {{-- <a href="/create-categoria">Criar Categoria</a>&nbsp; --}}
+            <a href="/create-marca">Criar Marca</a>&nbsp;
             <a href="/create-produto">Criar Produto</a>
 
         </div>
 
+        <div class="grid-container dash-pedido-produto margin-new">
+            <div class="ult-pedidos bg-gray border-10 padding-detalhes">
+                <div class="flex-container topo-secao">
+                    <i class="fa-solid fa-square red"></i>
+                    <h3>Pedidos Abertos</h3>
+                </div>
+
+                @if (count($pedidos) > 0)
+
+                @foreach ($pedidos as $pedido)
+                    <table>
+                        <caption>Pedido: {{$pedido->id}}</caption>
+                        <tr>
+                            <th>Data e hora</th>
+                            <th>Pagamento</th>
+                            <th>Valor</th>
+                            <th>Status</th>
+                        </tr>
+                        <tr>
+                            <td>{{$pedido->created_at}}</td>
+                            <td>{{$pedido->pagamento_tipo}}</td>
+                            <td>{{$pedido->total_pedido}}</td>
+                            <td>{{$pedido->status}}</td>
+                            <td><a href="/editar-pedido/{{$pedido->id}}">Editar Status</a></td>
+                        </tr>
+                    </table>
+
+                @endforeach
+
+                <a href="/todos-pedidos">
+                    <button class="bt-red">Visualizar todos os Pedidos</button>
+                </a>
+
+            @else
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                <h2>Ainda não foram realizados Pedidos</h2>
+            @endif
+
+            </div>
+            <div class= "todos-produtos bg-gray border-10 padding-detalhes">
+                <div class="flex-container topo-secao">
+                    <i class="fa-solid fa-square red"></i>
+                    <h3>Produtos</h3>
+                </div>
+
+                @if (count($produtos) > 0)
+
+                <table>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Opções</th>
+                    </tr>
+                    @foreach ($produtos as $produto)
+
+                            <tr>
+                                <td>{{$produto->nome}}</td>
+                                <td><a href="/editar-produto/{{$produto->id}}">Editar</a></td>
+                                <td><a href="/remover-produto/{{$produto->id}}">Excluir</a></td>
+                            </tr>
+
+                    @endforeach
+
+                </table>
+
+                <a href="/todos-produtos">
+                    <button class="bt-red">Visualizar todos os Produtos</button>
+                </a>
+
+            @else
+                <h2>Não há Produtos</h2>
+
+            @endif
+
+            </div>
+        </div>
+
+        {{-- versão 2 --}}
         <div class="flex-container secao black">
             <i class="fa-solid fa-square red"></i> &nbsp;
             <h3>Últimos Pedidos Abertos</h3>
--- OChardware/resources/views/usuarios/perfil.blade.php
+++ OChardware/resources/views/usuarios/perfil.blade.php
@@ -6,19 +6,58 @@
     <section class="flex-container corpo-detalhes">
         <section class="detalhes-topo bg-gray border-10 margin-new">
             <div class="grid-container detalhes-foto-nome">
-                <div class="flex-container detalhes-foto">
+                <div class="flex-container perfil-foto">
                     <img src="img/usuarios/{{Auth::user()->foto}}" class="border-10" alt="Perfil">
                 </div>
                 <div class="flex-container detalhes-nome">
-                    <h2>{{Auth::user()->nome}} {{Auth::user()->sobrenome}}</h2>
-                    <hr>
+                    <h1>{{Auth::user()->nome}} {{Auth::user()->sobrenome}}</h1>
+                    <div class="flex-container perfil-opcoes">
+                        <a href="/editar-perfil">Editar cadastro</a>&nbsp;
+                        <a href="/editar-perfil">Alterar Senha</a>&nbsp;
+                        <a href="/adicionar-endereco">Meus Endereços</a>
+                    </div>
+                    {{-- <hr>
                     <h3>Usuário desde: {{Auth::user()->created_at->format('d/m/Y')}}</h3>
                     <a href="/editar-perfil">Editar cadastro</a>
-                    <a href="/adicionar-endereco">Meus Endereços</a>
+                    <a href="/adicionar-endereco">Meus Endereços</a> --}}
 
                 </div>
             </div>
         </section>
+
+        <div class="detalhes-descricao bg-gray border-10 padding-detalhes margin-new">
+            <div class="flex-container topo-secao">
+                <i class="fa-solid fa-square red"></i>
+                <h3>Últimos Pedidos</h3>
+            </div>
+
+            @if (count($pedidos) > 0)
+
+                @foreach ($pedidos as $pedido)
+                    <table>
+                        <caption>Pedido: {{$pedido->id}}</caption>
+                        <tr>
+                            <th>Data e hora</th>
+                            <th>Pagamento</th>
+                            <th>Valor</th>
+                            <th>Status</th>
+                        </tr>
+                        <tr>
+                            <td>{{$pedido->created_at}}</td>
+                            <td>{{$pedido->pagamento_tipo}}</td>
+                            <td>{{$pedido->total_pedido}}</td>
+                            <td>{{$pedido->status}}</td>
+                        </tr>
+                    </table>
+                @endforeach
+
+            @else
+                <i class="fa-solid fa-triangle-exclamation"></i>
+                <h2>Ainda não foram realizadas Compras</h2>
+            @endif
+
+
+        </div>
 
         <div class="flex-container secao black">
             <i class="fa-solid fa-square red"></i> &nbsp;
