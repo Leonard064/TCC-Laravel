2f87b014829af4fdd1013990cbcee16cc8ea6d54
Leonard064 <leonardo.nascibarbosa@outlook.com>
2023-04-09 13:08:53 -0300

    Mudanças - Estilos Págs. Todos os Pedidos, Todos os Prod.

--- OChardware/resources/views/pedidos/showAdmPedidos.blade.php
+++ OChardware/resources/views/pedidos/showAdmPedidos.blade.php
@@ -4,12 +4,40 @@
 
 @section('conteudo')
     <section class="flex-container corpo">
-        <div class="flex-container secao black">
-            <i class="fa-solid fa-square red"></i> &nbsp;
-            <h2>Todos os Pedidos</h2>
+
+        <div class="todos-pedidos bg-gray border-10 margin-new padding-detalhes">
+            <div class="flex-container topo-secao">
+                <i class="fa-solid fa-square red"></i>
+                <h2>Todos os Pedidos</h2>
+            </div>
+
+            <div id="tabela-todos-pedidos">
+                @if (count($pedido) > 0)
+                    <table>
+                        <tr>
+                            <th>Data e hora</th>
+                            <th>Pagamento</th>
+                            <th>Valor</th>
+                            <th>Status</th>
+                        </tr>
+                    @foreach ($pedido as $pedidos)
+                        <tr>
+                            <td>{{$pedidos->created_at}}</td>
+                            <td>{{$pedidos->pagamento_tipo}}</td>
+                            <td>{{$pedidos->total_pedido}}</td>
+                            <td>{{$pedidos->status}}</td>
+                            <td><a href="/editar-pedido/{{$pedidos->id}}">Editar Status</a></td>
+                        </tr>
+                    @endforeach
+                    </table>
+                @else
+                    <i class="fa-solid fa-triangle-exclamation"></i>
+                    <h2>Ainda não foram realizados Pedidos</h2>
+                @endif
+            </div>
         </div>
 
-        @if (count($pedido) > 0)
+        {{-- @if (count($pedido) > 0)
 
                 @foreach ($pedido as $pedido)
                     <table>
@@ -34,6 +62,6 @@
             @else
                 <i class="fa-solid fa-triangle-exclamation"></i>
                 <h2>Ainda não foram realizados Pedidos</h2>
-            @endif
+            @endif --}}
     </section>
 @endsection
--- OChardware/resources/views/produtos/showAdmProdutos.blade.php
+++ OChardware/resources/views/produtos/showAdmProdutos.blade.php
@@ -4,10 +4,46 @@
 
 @section('conteudo')
     <section class="flex-container corpo">
-        <div class="flex-container secao black">
-            <i class="fa-solid fa-square red"></i> &nbsp;
-            <h2>Todos os Produtos</h2>
+
+        <div class="todos-produtos bg-gray border-10 margin-new padding-detalhes">
+            <div class="flex-container topo-secao">
+                <i class="fa-solid fa-square red"></i>
+                <h2>Todos os Produtos</h2>
+            </div>
+
+            <div id="tabela-todos-produtos">
+                @if(count($produto) > 0)
+                <table>
+                    <tr>
+                        <th>Foto</th>
+                        <th>Nome</th>
+                        <th>Quantidade</th>
+                        <th>Valor</th>
+                        <th>Opções</th>
+                    </tr>
+
+                    @foreach ($produto as $produtos)
+                        <tr>
+                            <td>{{$produtos->foto}}</td>
+                            <td>{{$produtos->nome}}</td>
+                            <td>{{$produtos->quantidade}}</td>
+                            <td>{{$produtos->preco}}</td>
+                            <td><a href="/editar-produto/{{$produtos->id}}">Editar</a></td>
+                            <td><a href="/remover-produto/{{$produtos->id}}">Excluir</a></td>
+                        </tr>
+                    @endforeach
+
+                </table>
+
+            @else
+                <h3>Não há produtos</h3>
+
+            @endif
+            </div>
+
         </div>
+
+
         <div class="flex-container margem">
 
             @if(count($produto) > 0)
